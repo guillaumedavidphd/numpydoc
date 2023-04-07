e790c68e800d07a6127b9e19eb5f5a7acced56b9
Guillaume David <guillaumedavidphd@users.noreply.github.com>
2023-04-07 09:00:02 -0700

    Merge from `numpy/numpydoc` (#1)
    * MAINT: Fix CIs for sphinx 6 (#448)
    
    * BUG: PR06 logic to only fail when type is used standalone (#447)
    
    Change PR06 logic to only fail when type is used standalone.
    
    This prevents failures for user-defined classes, e.g. Mystring
    
    Co-authored-by: Ross Barnowski <rossbar@berkeley.edu>
    
    * Update pre-commit (#449)
    
    * Update pre-commit
    
    * Run pre-commit on all files
    
    * Update blacken-docs repo in pre-commit conf after owner transfer. (#451)
    
    * MAINT: Compatibility with pydata-sphinx-theme (#453)
    
    Fix compatibility issues between latest pydata-sphinx-theme and sphinx 6.
    
    Increase granularity of CI to test across necessary sphinx & Python version combos.
    
    * MAINT: Refactoring `get_doc_object` (#452)
    
    ---------
    
    Co-authored-by: Eric Larson <larson.eric.d@gmail.com>
    Co-authored-by: Bryce Mecum <petridish@gmail.com>
    Co-authored-by: Ross Barnowski <rossbar@berkeley.edu>
    Co-authored-by: Jarrod Millman <jarrod.millman@gmail.com>
    Co-authored-by: Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
    Co-authored-by: yuki <126044367+F3eQnxN3RriK@users.noreply.github.com>

--- numpydoc/docscrape.py
+++ numpydoc/docscrape.py
@@ -408,7 +408,7 @@ class NumpyDocString(Mapping):
             msg = "Docstring contains a Receives section but not Yields."
             raise ValueError(msg)
 
-        for (section, content) in sections:
+        for section, content in sections:
             if not section.startswith(".."):
                 section = (s.capitalize() for s in section.split(" "))
                 section = " ".join(section)
@@ -631,7 +631,6 @@ class ObjDoc(NumpyDocString):
 
 
 class ClassDoc(NumpyDocString):
-
     extra_public_methods = ["__call__"]
 
     def __init__(self, cls, doc=None, modulename="", func_doc=FunctionDoc, config=None):
@@ -728,7 +727,15 @@ class ClassDoc(NumpyDocString):
         return True
 
 
-def get_doc_object(obj, what=None, doc=None, config=None):
+def get_doc_object(
+    obj,
+    what=None,
+    doc=None,
+    config=None,
+    class_doc=ClassDoc,
+    func_doc=FunctionDoc,
+    obj_doc=ObjDoc,
+):
     if what is None:
         if inspect.isclass(obj):
             what = "class"
@@ -742,10 +749,10 @@ def get_doc_object(obj, what=None, doc=None, config=None):
         config = {}
 
     if what == "class":
-        return ClassDoc(obj, func_doc=FunctionDoc, doc=doc, config=config)
+        return class_doc(obj, func_doc=func_doc, doc=doc, config=config)
     elif what in ("function", "method"):
-        return FunctionDoc(obj, doc=doc, config=config)
+        return func_doc(obj, doc=doc, config=config)
     else:
         if doc is None:
             doc = pydoc.getdoc(obj)
-        return ObjDoc(obj, doc, config=config)
+        return obj_doc(obj, doc, config=config)
